7eec60da90bf1d366154370892cfc229094e4712
minhtuyen248 <minhtuyen269@gmail.com>
2024-04-03 17:08:42 +0700

    HoanThanhBaiTap

---
+++ src/Main.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+//        Bài 1: Nhập vào hai số a và b, và kiểm tra xem a có chia hết cho b hay không.
+//        System.out.println("Nhập số a: ");
+//        int a = scanner.nextInt();
+//        System.out.println("Nhập số b: ");
+//        int b = scanner.nextInt();
+//        if (a % b == 0) {
+//            System.out.println("Số a chia hết cho b");
+//        } else {
+//            System.out.println("Số a không chia hết cho b");
+//        }
+//        scanner.close();
+
+//        Bài 2: Nhập tuổi và in ra kết quả nếu tuổi học sinh đó không đủ điều kiện vào học lớp 10.
+//        System.out.println("Nhập tuổi : ");
+//        int a = scanner.nextInt();
+//        if (a > 15) {
+//            System.out.println("Bạn đủ tuổi vào lớp 10");
+//        } else {
+//            System.out.println("Bạn chưa đủ tuổi vào lớp 10");
+//        }
+
+//        Bài 3: Nhập một số nguyên bất kỳ và in kết quả ra màn hình để nói cho người dùng biết số đó là lớn hay nhỏ hơn 0
+//        System.out.println("Nhập số : ");
+//        int number = scanner.nextInt();
+//        if (number < 0) {
+//            System.out.println("Số bạn nhập nhỏ hơn 0");
+//        } else if (number > 0) {
+//            System.out.println("Số bạn nhập lớn hơn 0");
+//        }
+
+//        Bài 4: Nhập 3 số nguyên và tìm giá trị lớn nhất của ba số nguyên đó
+//        System.out.println("Nhập số a: ");
+//        int a = scanner.nextInt();
+//        System.out.println("Nhập số b: ");
+//        int b = scanner.nextInt();
+//        System.out.println("Nhập số c: ");
+//        int c = scanner.nextInt();
+//
+//        if (a > b && a > c) {
+//            System.out.println("Số a lớn nhất");
+//        } else if (b > c) {
+//            System.out.println("Số b lớn nhất");
+//        }else {
+//            System.out.println("Số c lớn nhất");
+//        }
+
+//        Bài 5: Xếp hạng học lực của học sinh dựa trên các điểm bài kiểm tra, điểm thi giữa kỳ, điểm thi cuối kỳ
+//        System.out.println("Nhap diem: ");
+//        int number = scanner.nextInt();
+//        if (number < 4) {
+//            System.out.println(number + " bạn học lực yếu vl ");
+//        } else if (number >= 4 && number < 6) {
+//            System.out.println(number + " bạn học lực trung bình");
+//        } else if (number >= 6 && number < 8) {
+//            System.out.println(number + " bạn học lực khá");
+//        } else if (number >= 8 && number <= 9) {
+//            System.out.println(number + "bạn học lực giỏi");
+//        } else if (number > 9 && number < 10) {
+//            System.out.println(number + "bạn học lực xuất sắc VL ");
+//        }
+//
+//        scanner.close();
+
+//        Bài 6: Tính hoa hồng nhận được tuỳ theo mức doanh số bán hàng
+//        System.out.println("Nhập doanh số đã bán: ");
+//        int number = scanner.nextInt();
+//
+//        if (number < 500) {
+//            System.out.println("Hoa hồng của bạn là : " + 5 + "%");
+//        } else if (number >= 500 && number < 3000) {
+//            System.out.println("Hoa hồng của bạn là : " + (number * 0.05 - 499) + number * 0.08 + "%");
+//        } else if (number >= 3000) {
+//            System.out.println("Hoa hồng của bạn là : " + number * 0.1 + "%");
+//        }
+
+//        Bài 7: Tính cước điện thoại cho một hộ gia đình với các thông số đã cho
+//        System.out.println("Nhập số phút cuộc gọi bạn đã sử dụng: ");
+//        int number = scanner.nextInt();
+//
+//        if (number < 50) {
+//            System.out.println("Cước phí bạn đã sử dụng" + number * 0.2);
+//        } else if (number > 50 && number < 150) {
+//            System.out.println();
+//        }
+
+//        Bài 1: Chuyển từ độ C sang độ F. °C  x  9/5 + 32 = °F
+//        System.out.print("Nhập độ C: ");
+//        int number = scanner.nextInt();
+//
+//        System.out.printf("Nhiệt độ F: " + ((number * 9)/5 + 32) + "°F");
+
+//        Bài 2: Chuyển từ mét sang feet: ft =m * 3.2808
+//        System.out.printf("Nhập mét: ");
+//        int number = scanner.nextInt();
+//        System.out.println("feet là : " + number * 3.2808);
+
+
+
+    }
+}
